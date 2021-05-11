be96a189cd6b992d8f6e159802e20ddaaa683df2
Jim Tyson <1130589+jimbotyson@users.noreply.github.com>
2021-05-11 06:40:10 +0100

    Add files via upload

---
+++ sum_ints.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 10 13:43:09 2021
+
+@author: Jim Tyson
+
+Convert the following python function so that it can sum a
+vector of integers.
+
+"""
+
+
+def sum_ints(intone, inttwo):
+    return(intone + inttwo)
+
